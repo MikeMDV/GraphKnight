ad98d030def5244563cf831a8d25ce1b5c65d27c
Michael Marven <mmarven09@gmail.com>
2017-05-31 08:48:24 -0700

    Update KnightGraph.h

--- Level2/KnightGraph.h
+++ Level2/KnightGraph.h
@@ -23,15 +23,15 @@ public:
     // Destructor
     ~KnightGraph();
 
-    /* Brief desc.       - A method to build a graph of knight moves and a path 
-     *                     to the end using depth-first search
+    /* Brief desc.       - A method to find a path to the end using depth-first 
+     *                     search
      * param[in] x_start - X coordinate of the starting node
      * param[in] y_start - Y coordinate of the starting node
      * param[in] x_end   - X coordinate of the ending node
      * param[in] y_end   - Y coordinate of the ending node
      *
      */
-    void dfsGraphBuild(int start_x, int start_y, int end_x, int end_y);
+    void dfsPathFind(int start_x, int start_y, int end_x, int end_y);
 
     /* Brief desc.       - A method to retrieve the adjacency matrix showing the 
      *                     connections between nodes in the graph
@@ -39,7 +39,7 @@ public:
      */
     std::vector<std::vector<bool> > getAdjMatrix();
 
-    /* Brief desc.       - A method to retrieve the path of move to the end node 
+    /* Brief desc.       - A method to retrieve the path of moves to the end node 
      *
      */
     std::vector<Vertex> getPathToEnd();
@@ -47,16 +47,21 @@ public:
 
 private:
 
-
+    /* Brief desc.      - A recursive method to visit nodes and return a vector
+     *                    of Vertex structs to build the path to the end node
+     * param[in] 
+     *
+     */
+    std::vector<Vertex> visitNext(int start_x, int start_y, int end_x, int end_y);
 
     // Attributes
+    MoveValidator m_validator;
+    
     std::vector<std::vector<char> > m_board;
 
     std::vector<Vertex> m_nodes;
 
     std::vector<Vertex> m_path;
-
-    std::vector<std::vector<bool> > m_adj_matrix;
 
 };
 
